649517b1fe67d973186fcc10a92671f5ab098a64
sambaker149 <91675987+sambaker149@users.noreply.github.com>
2022-11-24 14:18:16 +0000

    Added DbInitializer

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using CO550TeamProject.Models;
+
+namespace CO550TeamProject.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            // Look for any students.
+            if (context.Customer.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var customers = new Customer[]
+            {
+                new Customer{Name="",DateOfBirth=,Email="",PhoneNumber=""}
+            };
+
+            context.Customer.AddRange(customers);
+            context.SaveChanges();
+
+            var customerAddresses = new CustomerAddress[]
+            {
+                new CustomerAddress{HouseNumber=,HouseName="",RoadName="",PostTown="",Postcode=""}
+            };
+
+            context.CustomerAddress.AddRange(customerAddresses);
+            context.SaveChanges();
+
+            var customerPayments = new CustomerPayment[]
+            {
+                new CustomerPayment{CardNumber=,ExpiryMonth=,ExpiryYear=,CVVNumber=}
+            };
+
+            context.CustomerPayment.AddRange(customerPayments);
+            context.SaveChanges();
+        }
+    }
+}
